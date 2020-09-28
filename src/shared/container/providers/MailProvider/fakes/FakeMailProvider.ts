3e4e526e94fa9a5f539a0bdf8cdf84e8259d1cba
Lucas Pinheiro <lucas.pbezerra@hotmail.com>
2020-09-28 18:06:15 -0300

    Email templates

--- src/shared/container/providers/MailProvider/fakes/FakeMailProvider.ts
+++ src/shared/container/providers/MailProvider/fakes/FakeMailProvider.ts
@@ -1,14 +1,10 @@
-import IMailProvider from "../models/IMailProvider";
-
-interface IMessage {
-    to: string;
-    body: string;
-}
+import ISendMailDTO from '../dtos/ISendMailDTO';
+import IMailProvider from '../models/IMailProvider';
 
 export default class FakeMailProvider implements IMailProvider {
-    private messages: IMessage[] = [];
+  private messages: ISendMailDTO[] = [];
 
-    public async sendMail(to: string, body: string) {
-        this.messages.push({ to, body });
-    }
+  public async sendMail(message: ISendMailDTO) {
+    this.messages.push(message);
+  }
 }
